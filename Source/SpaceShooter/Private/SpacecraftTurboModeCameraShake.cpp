c602b1ded2ad676e5f1e1f8dd38db2ac8b20dbef
Vladut Buhoci <buhoci.vlad@gmail.com>
2019-05-15 01:38:48 +0300

    - reduced camera shake amplitude which is played when the spacecraft enters turbo mode; - added "Weapon Overheated!" message on the screen if the equipped weapon is overheated; - added a weapon/ammo type and units left display.

--- Source/SpaceShooter/Private/SpacecraftTurboModeCameraShake.cpp
+++ Source/SpaceShooter/Private/SpacecraftTurboModeCameraShake.cpp
@@ -5,24 +5,24 @@
 
 USpacecraftTurboModeCameraShake::USpacecraftTurboModeCameraShake()
 {
-	OscillationDuration            = 0.35f;
+	OscillationDuration            = 0.20f;
 	
 	RotOscillation.Roll.Amplitude  = 2.0f;
-	RotOscillation.Roll.Frequency  = 10.0f;
+	RotOscillation.Roll.Frequency  = 8.0f;
 
 	RotOscillation.Pitch.Amplitude = 2.0f;
-	RotOscillation.Pitch.Frequency = 10.0f;
+	RotOscillation.Pitch.Frequency = 7.5f;
 
 	RotOscillation.Yaw.Amplitude   = 2.0f;
-	RotOscillation.Yaw.Frequency   = 10.0f;
+	RotOscillation.Yaw.Frequency   = 8.0f;
 
-	LocOscillation.X.Amplitude     = 5.0f;
-	LocOscillation.X.Frequency     = 20.0f;
+	LocOscillation.X.Amplitude     = 3.0f;
+	LocOscillation.X.Frequency     = 15.0f;
 
-	LocOscillation.Y.Amplitude     = 5.0f;
-	LocOscillation.Y.Frequency     = 20.0f;
+	LocOscillation.Y.Amplitude     = 3.0f;
+	LocOscillation.Y.Frequency     = 15.0f;
 
-	LocOscillation.Z.Amplitude     = 10.0f;
-	LocOscillation.Z.Frequency     = 20.0f;
+	LocOscillation.Z.Amplitude     = 5.0f;
+	LocOscillation.Z.Frequency     = 15.0f;
 	LocOscillation.Z.InitialOffset = EInitialOscillatorOffset::EOO_OffsetZero;
 }
